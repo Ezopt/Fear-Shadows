195de0a60dde689178f7c72748939f2378938742
Synx0o <synxoo@mailoo.org>
2016-04-04 14:57:46 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -11,9 +11,41 @@ pygame.init()
 while True:
 	fond.blit(accueil,(0,0))
 	pygame.display.flip()
+	
 	for event in pygame.event.get() :
 		if event.type == KEYDOWN :
-			if event.key = K_ENTER :
+			if event.key == K_p :
+				fond.blit(persos,(0,0))
+				if event.key == K_s :
+					choix_perso = 1
+				if event.key == K_a :
+					choix_perso = 2
+				if event.key == K_v :
+					choix_perso = 3
+				if event.key == K_u :
+					choix_perso = 4
+				if event.key == K_c :
+					choix_perso = 5
+				else :
+					choix_perso = 1
+			if event.key == K_m :
+				fond.blit(monstres,(0,0))
+				if event.key == K_b :
+					choix_monstre = 1
+				if event.key == K_w :
+					choix_monstre = 2
+				if event.key == K_z :
+					choix_monstre = 3
+				if event.key == K_m :
+					choix_monstre = 4
+				if event.key == K_c :
+					choix_monstre = 5
+				if event.key == K_l :
+					choix_monstre = 6
+				else :
+					choix_perso = 1
+				
+			if event.key == K_ENTER :
 				fonction_jeu.fearshadows
 		if event.type == QUIT
 			pygame.quit()
