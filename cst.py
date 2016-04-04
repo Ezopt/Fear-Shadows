6a614451ea164899e53abf7c3088cc44d09c7e60
Synx0o <synxoo@mailoo.org>
2016-04-04 14:39:07 +0200

    Update cst.py

--- cst.py
+++ cst.py
@@ -1,1 +1,24 @@
-fond = pygame.image.load("./Img_FS/back1.jpg").convert()
+fond = pygame.image.load(image_fond).convert()
+
+#Paramètres de la fenêtre
+
+nombre_sprite_cote = 15
+
+taille_sprite = 30
+
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
+
+#Personnalisation de la fenêtre
+
+titre_fenetre = "Fear Shadows"
+
+
+
+#Listes des images du jeu
+
+image_accueil = "images/accueil.png"
+
+image_fond = "./Img_FS/back1.jpg"
+
+image_mur = "images/mur.png"
