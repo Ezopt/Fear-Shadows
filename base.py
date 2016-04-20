6439ca1e8f65a5fd38b19a111d72d9cfee2f192c
Synx0o <synxoo@mailoo.org>
2016-04-20 22:30:03 +0200

    Update base.py

--- base.py
+++ base.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Éditeur de Spyder
+Created on Wed Apr 20 16:00:15 2016
 
-Ceci est un script temporaire.
+@author: KB
 """
+
 #Importation des bibliothèques Pygame 
 import pygame
 from pygame.locals import *
-from menu import *
-from classes import *
+#from classes import *
 from cst import *
 
 #PENSER A MODIFIER LES VALEURS POUR LA FENÊTRE ET AUTRES, EN 32 PAR 32
@@ -19,9 +19,10 @@ pygame.init()
 #Création de la fenêtre
 fenetre = pygame.display.set_mode((1344, 800))
 
-perso = pygame.image.load("./Img_FS/p"choix_perso".png").convert_alpha()
-
-monstre = pygame.image.load("./Img_FS/m"choix_monstre".png").convert_alpha()
+fond = pygame.image.load(image_fond).convert()
+accueil = pygame.image.load(image_accueil).convert()
+persos = pygame.image.load(image_persos).convert()
+monstres = pygame.image.load(image_monstres).convert()
 
 x1_perso = 32
 y1_perso = 704
@@ -35,18 +36,123 @@ y2_monstre = y1_monstre + 32
 
 actif = {K_s: False, K_w: False, K_a: False, K_d: False, K_UP: False, K_DOWN: False, K_RIGHT: False, K_LEFT: False}
 
-#Positionnement des images sur l'écran
-fenetre.blit(fond, (0,0))
-fenetre.blit(perso, (x1_perso, y1_perso))
-fenetre.blit(monstre,(x1_monstre, y1_monstre))
-
-#Rafraîchissement de l'image
-pygame.display.flip()
 
 pygame.key.set_repeat(400, 100)
 
 open = True
 while open:
+
+    menu = True
+    reglages = False
+    p = False
+    m = False
+    choix_perso = 0
+    choix_monstre = 0
+
+    #penser à importer les variables de surface, la création, etc.
+    while menu == True:
+	 
+      fenetre.blit(accueil, (0,0))
+      pygame.display.flip()
+      
+      if choix_perso==0:
+          choix_perso = 1
+      if choix_monstre==0:
+          choix_monstre = 1
+      
+
+      for event in pygame.event.get() :
+          if event.type == QUIT :
+                 pygame.quit()
+                 quit()
+          if event.type == KEYDOWN :
+                if event.key == K_p :
+                    reglages = True
+                    p = True
+                if event.key == K_SEMICOLON :
+                    reglages = True
+                    m = True
+                if event.key == K_RETURN :
+                    menu = False
+
+	
+      while reglages == True :
+		
+          if p == True :
+                fenetre.blit(persos,(0,0))
+                pygame.display.flip()
+                for event in pygame.event.get() :
+                    if event.type == QUIT :
+                            pygame.quit()
+                            quit()
+				
+                    if event.type == KEYDOWN :
+			
+                            if event.key == K_s :
+                                choix_perso = 1
+                                reglages = False
+                            if event.key == K_q :
+                                choix_perso = 2
+                                reglages = False
+                            if event.key == K_v :
+                                choix_perso = 3
+                                reglages = False
+                            if event.key == K_u :
+                                choix_perso = 4
+                                reglages = False
+                            if event.key == K_c :
+                                choix_perso = 5
+                                reglages = False
+                            if event.key == K_ESCAPE :
+                                reglages = False
+                            else :
+                                choix_perso = 1
+          elif m == True :
+                fenetre.blit(monstres,(0,0))
+                pygame.display.flip()
+                for event in pygame.event.get() :
+                    if event.type == QUIT :
+                            pygame.quit()
+                            quit()
+       
+                    if event.type == KEYDOWN :
+					
+                            if event.key == K_b :
+                                choix_monstre = 1
+                                reglages = False
+                            if event.key == K_z :
+                                choix_monstre = 2
+                                reglages = False
+                            if event.key == K_w :
+                                choix_monstre = 3
+                                reglages = False
+                            if event.key == K_SEMICOLON :
+                                choix_monstre = 4
+                                reglages = False
+                            if event.key == K_c :
+                                choix_monstre = 5
+                                reglages = False
+                            if event.key == K_l :
+                                choix_monstre = 6
+                                reglages = False
+                            if event.key == K_ESCAPE :
+                                reglages = False
+                            else :
+                                choix_monstre = 1
+
+    perso = pygame.image.load(dossier + "p" +str(choix_perso) + extension1).convert_alpha()
+
+    monstre = pygame.image.load(dossier + "m" + str(choix_monstre) + extension1).convert_alpha()
+    
+    
+    #Positionnement des images sur l'écran
+    fenetre.blit(fond, (0,0))
+    fenetre.blit(perso, (x1_perso, y1_perso))
+    fenetre.blit(monstre,(x1_monstre, y1_monstre))
+    
+    #Rafraîchissement de l'image
+    pygame.display.flip()
+    
     for event in pygame.event.get():   #On parcours la liste de tous les événements reçus
             
             if event.type == KEYDOWN:
